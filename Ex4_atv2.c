6fc55a7da31c3b1b93268d15bf15e91cc99ca4f6
Davicmota7 <135145240+Davicmota7@users.noreply.github.com>
2023-09-07 12:55:33 -0300

    Add files via upload

---
+++ Ex4_atv2.c
@@ -0,0 +1,35 @@
+int main() {
+    int a, b, c, menor, maior;
+
+    printf("Digite tres valores: ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if(a < b){
+        if(a < c)
+            menor = a;
+        else
+            menor = c;
+    }
+    else{
+        if(b < c)
+            menor = b;
+        else
+            menor = c;
+    }
+
+    
+    if(a > b){
+        if(a > c)
+            maior = a;
+        else
+            maior = c;
+    }
+    else{
+        if(b > c)
+            maior = b;
+        else
+            maior = c;
+    }
+
+    printf("Primeira versao\tMenor: %d\tMaior: %d\n", menor, maior);
+}
